2c11559d0ea5b03ee8b724985513eabb5d080155
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-02-08 17:50:44 +0300

    click_generator_with_subs

---
+++ click.py
@@ -0,0 +1,48 @@
+from subs_generator import *
+import requests
+from urllib.parse import urlparse
+
+
+subs = {'sub1':generate_sub1(),
+        'sub2':generate_sub2(),
+        'sub3':generate_sub3(),
+        'sub4':generate_sub4(),
+        'sub5':generate_sub5()
+        }
+link = 'http://lawn.mobstra.com/go?id=591&hash=FG4crGUwMN'
+
+
+r = requests.get(link,subs)
+
+def get_responce_url():
+    return r.text
+
+parsed_url = urlparse(r.text)
+
+def apend_click_from_link():
+    return parsed_url.path[1:]
+
+def set_click_param():
+    click = apend_click_from_link()
+    return click
+
+
+# print(set_click_param())
+
+
+# def string_from_link(self):
+#     self.converted_link_value = str(converted_link_value)
+#     return print(str(converted_link_value))
+
+# print(r.status_code)
+# print(r.text)
+
+
+
+
+
+
+
+
+
+
---
+++ subs_generator.py
@@ -0,0 +1,37 @@
+import random
+
+
+def generate_sub1():
+    random_int = random.randint(1, 9999999)
+    random_str = str(random_int) + 'sub' + str(random_int)
+    sub1 = random_str
+    return sub1
+
+def generate_sub2():
+    random_int = random.randint(1, 9999999)
+    random_str = str(random_int) + 'sub' + str(random_int)
+    sub2 = random_str
+    return sub2
+
+def generate_sub3():
+    random_int = random.randint(1, 9999999)
+    random_str = str(random_int) + 'sub' + str(random_int)
+    sub3 = random_str
+    return sub3
+
+def generate_sub4():
+    random_int = random.randint(1, 9999999)
+    random_str = str(random_int) + 'sub' + str(random_int)
+    sub4 = random_str
+    return sub4
+
+def generate_sub5():
+    random_int = random.randint(1, 9999999)
+    random_str = str(random_int) + 'sub' + str(random_int)
+    sub5 = random_str
+    return sub5
+
+# print(generate_sub1(),generate_sub2(),generate_sub3(),generate_sub4(),generate_sub5())
+
+
+
