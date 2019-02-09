886bdf1bb877b8f85bceca80f17104c4c06b1ba5
obezludko <3735399Qq>
2019-02-09 10:13:48 +0300

    package creating + files mover into package

--- lead/lead_params_generating.py
+++ lead/lead_params_generating.py
@@ -1,15 +1,6 @@
 import random
 from click_sub.click import set_click_param
 
-
-
-
-external_message_id = str
-external_subscription_id = str
-currency = str
-payout = float
-extra_param = str
-
 def generate_partner():
     partner_list = ['nouniqlead','test', 'test_reliz_shark',
                     'wapclick', 'u', 'testignore', 'leadtestignore',
@@ -23,5 +14,28 @@ def define_click_for_lead():
     click_for_lead = set_click_param()
     return click_for_lead
 
+def lead_external_message_id():
+    external_message_id = set_click_param() + 'q' + str(random.randint(1,1000))
+    return external_message_id
 
-print(())
+def lead_external_subscription_id():
+    external_subscription_id = set_click_param() + 'w' + str(random.randint(1,1000))
+    return external_subscription_id
+
+def currency_choice():
+    currency_list = ['EUR','USD',
+                     'ALL','KWD']
+    random_currency = random.choice(currency_list)
+    return random_currency
+
+def payout_choice():
+    payout = random.randint(0,15)
+    return str(payout)
+
+def lead_extra_param():
+    extra_param = 'extra ' + str(random.randint(1,10)) + ' param'
+    return extra_param
+
+print(lead_extra_param())
+
+
