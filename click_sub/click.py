b68498abff492efa881ffe3dddc936ff26a19a7e
obezludko <3735399Qq>
2019-02-17 12:31:29 +0300

    edit sub_generator function. add it in click generation

--- click_sub/click.py
+++ click_sub/click.py
@@ -3,11 +3,11 @@ import requests
 from urllib.parse import urlparse
 
 
-subs = {'sub1':generate_sub1(),
-        'sub2':generate_sub2(),
-        'sub3':generate_sub3(),
-        'sub4':generate_sub4(),
-        'sub5':generate_sub5()
+subs = {'sub1':generate_sub('sub1'),
+        'sub2':generate_sub('sub2'),
+        'sub3':generate_sub('sub3'),
+        'sub4':generate_sub('sub4'),
+        'sub5':generate_sub('sub5')
         }
 link = 'http://lawn.mobstra.com/go?id=591&hash=FG4crGUwMN'
 
--- click_sub/subs_generator.py
+++ click_sub/subs_generator.py
@@ -5,34 +5,10 @@ import random
 """
 
 
-def generate_sub1():
+def generate_sub(sub_gen):
+    sub_gen = str(sub_gen)
     random_int = random.randint(1, 9999999)
-    random_str = str(random_int) + ' sub 1 ' + str(random_int)
-    sub1 = random_str
-    return sub1
-
-def generate_sub2():
-    random_int = random.randint(1, 9999999)
-    random_str = str(random_int) + ' sub 2 ' + str(random_int)
-    sub2 = random_str
-    return sub2
-
-def generate_sub3():
-    random_int = random.randint(1, 9999999)
-    random_str = str(random_int) + ' sub 3 ' + str(random_int)
-    sub3 = random_str
-    return sub3
-
-def generate_sub4():
-    random_int = random.randint(1, 9999999)
-    random_str = str(random_int) + ' sub 4 ' + str(random_int)
-    sub4 = random_str
-    return sub4
-
-def generate_sub5():
-    random_int = random.randint(1, 9999999)
-    random_str = str(random_int) + ' sub 5 ' + str(random_int)
-    sub5 = random_str
-    return sub5
-
-
+    random_sub = str(random_int) \
+                 +' '+ sub_gen +\
+                 ' ' +str(random_int)
+    return random_sub
