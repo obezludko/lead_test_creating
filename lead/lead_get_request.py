0a5c18616e32ac03fb4cbcd9e8fb6135a8e2de8c
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-02-20 17:27:37 +0300

    some changes

--- lead/lead_get_request.py
+++ lead/lead_get_request.py
@@ -3,7 +3,7 @@ import DB.saved_variables
 
 link = 'http://159.69.18.119/lead?'
 lead_params = {
-    'payout':DB.saved_variables.partner,
+    'payout':DB.saved_variables.payout,
     'extra_param':DB.saved_variables.extra_param,
     'click_id':DB.saved_variables.click_id,
     'partner':DB.saved_variables.partner,
@@ -14,7 +14,7 @@ lead_params = {
 
 requests.get(link,lead_params)
 
-print('payout:' + DB.saved_variables.partner,
+print('payout:' + DB.saved_variables.payout,
     '\nextra_param:' + DB.saved_variables.extra_param,
     '\nclick_id:' + DB.saved_variables.click_id,
     '\npartner:' + DB.saved_variables.partner,
