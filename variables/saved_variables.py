98f432ac06ee7394e3583b6cc889ffe7b1775493
obezludko <3735399Qq>
2019-02-20 21:49:55 +0300

    some changes

--- variables/saved_variables.py
+++ variables/saved_variables.py
@@ -5,6 +5,7 @@ Saved params for:
  1) lead generating
  2) using in select queries from database
 '''
+
 partner = lead_params_generating.LeadQueryParams.generate_partner()
 click_id = lead_params_generating.LeadQueryParams.get_click_for_lead()
 external_message_id = lead_params_generating.LeadQueryParams.get_lead_external_message_id()
