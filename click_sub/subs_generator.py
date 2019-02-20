b4ec5f5167e56d4d6b56ed4987371170eb2f10e7
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-02-20 15:36:09 +0300

    some changes

--- click_sub/subs_generator.py
+++ click_sub/subs_generator.py
@@ -5,7 +5,7 @@ import random
 """
 
 
-def generate_sub(sub_gen):
+def generate_sub(sub_gen = 'sub value'):
     sub_gen = str(sub_gen)
     random_int = random.randint(1, 9999999)
     random_sub = str(random_int) \
